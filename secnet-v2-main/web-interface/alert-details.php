917ba5eec5ec4677688f3142efd3ad3e0c301e8c
Diego Pérez <diego.perz.garc@gmail.com>
2025-06-05 10:54:26 +0200

    Add files via upload

--- secnet-v2-main/web-interface/alert-details.php
+++ secnet-v2-main/web-interface/alert-details.php
@@ -41,6 +41,7 @@ if ($source_ip) {
     <title>Detalles de la Alerta</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&display=swap" rel="stylesheet">
     <style>
         /* Estilos para el modal */
         .modal {
