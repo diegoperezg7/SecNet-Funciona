2e2805fd762c1c22664e0484c47e4cbc796bf5b2
Diego Pérez <diego.perz.garc@gmail.com>
2025-06-05 18:31:21 +0200

    Add files via upload

--- secnet-v2-main/web-interface/alert-details.php
+++ secnet-v2-main/web-interface/alert-details.php
@@ -38,7 +38,8 @@ if ($source_ip) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalles de la Alerta</title>
+    <link rel="icon" type="image/png" href="assets/logo.png">
+    <title>Detalles de Alerta - SecNet</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&display=swap" rel="stylesheet">
--- secnet-v2-main/web-interface/alerts.php
+++ secnet-v2-main/web-interface/alerts.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alertas - Sistema de Respuesta a Incidentes de Seguridad</title>
+    <link rel="icon" type="image/png" href="assets/logo.png">
+    <title>Alertas - SecNet</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&display=swap" rel="stylesheet">
